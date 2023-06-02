4f9435540a88d398ba37cc9340585f7d1a23dcb3
miamarichka <ilchyshyn.marichka@gmail.com>
2023-06-02 13:22:10 +0300

    disable scroll

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,7 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const body = document.querySelector('body');
   
     menuBtnRef.addEventListener("click", () => {
       const expanded =
@@ -10,6 +11,7 @@
       menuBtnRef.setAttribute("aria-expanded", !expanded);
   
       mobileMenuRef.classList.toggle("is-open");
+      body.classList.toggle("is-hidden");
     });
   })();
 
@@ -19,9 +21,11 @@
     const closeMenuBtn = document.querySelector('.menu-close');
     const toggleMenu = () => {
       const isMenuOpen =
-        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
       mobileMenu.classList.toggle('is-open');
+      body.classList.toggle("is-hidden");
+      
       const scrollLockMethod = !isMenuOpen
         ? 'disableBodyScroll'
         : 'enableBodyScroll';
@@ -29,11 +33,11 @@
     };
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
-    // Закрываем мобильное меню на более широких экранах
-    // в случае изменения ориентации устройства.
+
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
       if (!e.matches) return;
       mobileMenu.classList.remove('is-open');
+      body.classList.remove("is-hidden");
       openMenuBtn.setAttribute('aria-expanded', false);
       bodyScrollLock.enableBodyScroll(document.body);
     });
--- js/modal.js
+++ js/modal.js
@@ -3,17 +3,19 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.modal.addEventListener("click", (e) => {
+    if (e.target === e.currentTarget) {
+      toggleModal();
+    }
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("is-hidden")
   }
 })();
-
-
-
-
-
